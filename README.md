f1496c6bb8bbda40e5d3ddb8eceb13304e3b53f8
John Hayes <johnhayesru@gmail.com>
2018-11-26 02:32:52 +0300

    iniitial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Bootstrap 4 Starter Kit
+
+This is the starter kit for Bootstrap 4.1.3. 
+
+---
+
+# How To Run
+Type the following into your terminal window:
+```
+npm install
+```
+Then type:
+```
+npm start
+```
+
+## Sass Compiling/Minification
+Gulp-sass handles compiling sass files and minifying them into css/main.min.css. If you wish to not use sass, rename css/main.min.css to main.css and include your own styling within that file. Also, change the stylesheet to main.css within the index.html file.
+
+## Image Minification
+Gulp-imagemin takes all images from src/img and minifies them. The minified images are then moved to dist/img for use within your project.
+
+## Live Server
+Browser-sync is used to watch for changes to any css or html file. The browser will reload on any change. In order to use live reload, use
+`localhost:3000` within your browser's address bar. The port may change, so make sure to check which port is being used within your terminal window.
+
+## Dependecies
+* Font Awesome 5.5.0
+* Bootstrap 4.1.3
+* Animate.css
+* Main.min.css
+* jQuery 3.3.1
+* Popper.js
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp'),
+  images = require('gulp-imagemin'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  watch = require('gulp-watch'),
+  rename = require('gulp-rename'),
+  browserSync = require('browser-sync').create();
+
+
+// Compiles all SASS files
+gulp.task('sass', () => {
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'compressed',
+    }))
+    .pipe(rename({
+      basename: 'main',
+      suffix: '.min',
+    }))
+
+    .pipe(gulp.dest('dist/css'));
+});
+
+// Minify all images
+gulp.task('images', () => {
+  return gulp.src('src/img/**/*')
+    .pipe(images())
+    .pipe(gulp.dest('dist/img'));
+});
+
+// Watch for changes
+gulp.task('watch', () => {
+
+  // Serve files
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    open: false,
+    notify: false,
+  });
+
+  gulp.watch('src/sass/**/*.scss', ['sass']);
+  gulp.watch('src/img/**/*', ['images']);
+  gulp.watch(['dist/*.html', 'dist/css/*.css']).on('change', browserSync.reload);
+});
+
+// Use default task to launch Browsersync
+gulp.task('default', ['sass', 'images', 'watch'], () => {});
